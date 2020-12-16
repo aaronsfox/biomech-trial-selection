3f109bc9bcec2a9dc6b612317b20f6bc780376df
Aaron Fox <48617376+aaronsfox@users.noreply.github.com>
2020-12-16 20:37:02 +1100

    Added tidy codes for walking and balance datasets.

--- Code/tidyDosSantos2017-Balance.py
+++ Code/tidyDosSantos2017-Balance.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Nov 30 15:43:19 2020
+Created on Tue Dec 15 21:09:07 2020
 
 @author:
     Aaron Fox
@@ -8,7 +8,7 @@ Created on Mon Nov 30 15:43:19 2020
     Deakin University
     aaron.f@deakin.edu.au
     
-    This function tidied up the Fukuchi et al. (2017) running C3D data files into
+    This function tidied up the dos Santos et al. (2017) balance data files into
     individual participant folders for later access. After running once the code will 
     no longer work, and simply served as a way to initially manage the dataset.
     
@@ -23,22 +23,22 @@ import shutil
 # %% Tidy dataset
 
 #Navigate to dataset folder
-os.chdir('..\\Fukuchi2017-RunningDataset')
+os.chdir('..\\DosSantos2017-BalanceDataset')
 
 #Identify subject list based on filenames
 #Get file list of static trials
 staticTrials = list()
-for file in glob.glob('*static.c3d'):
+for file in glob.glob('*static.txt'):
     staticTrials.append(file)
     
 #Loop through static trials and get participant list
 subList = list()
 for ii in range(len(staticTrials)):
     #Split string and append to list
-    subList.append(staticTrials[ii].split('static.c3d')[0])
+    subList.append(staticTrials[ii].split('static')[0])
     
 #Loop through participants and allocate files to folder
-for ii in range(len(subList)):
+for ii in range(1,len(subList)):
     
     #Create blank list for files
     subFiles = list()
@@ -50,9 +50,14 @@ for ii in range(len(subList)):
     #Create directory for current subject
     os.mkdir(subList[ii])
     
-    #Loop through file list and move
+    #Loop through file list
+    #Move if an angle or grf file
+    #Delete if a marker or static file
     for ff in range(len(subFiles)):
-        shutil.move(subFiles[ff],subList[ii]+'\\'+subFiles[ff])
+        if 'ang.txt' in subFiles[ff] or 'grf.txt' in subFiles[ff]:
+            shutil.move(subFiles[ff],subList[ii]+'\\'+subFiles[ff])
+        elif 'mkr.txt' in subFiles[ff] or 'static.txt' in subFiles[ff]:
+            os.remove(subFiles[ff])
+            
     
-# %% ----- End of tidyFukuchi2017-Running.py -----
-    
+# %% ----- End of tidyDosSantos2017-Balance.py -----
--- Code/tidyFukuchi2018-Walking.py
+++ Code/tidyFukuchi2018-Walking.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Nov 30 15:43:19 2020
+Created on Tue Dec 15 21:09:07 2020
 
 @author:
     Aaron Fox
@@ -8,7 +8,7 @@ Created on Mon Nov 30 15:43:19 2020
     Deakin University
     aaron.f@deakin.edu.au
     
-    This function tidied up the Fukuchi et al. (2017) running C3D data files into
+    This function tidied up the Fukuchi et al. (2018) walking C3D data files into
     individual participant folders for later access. After running once the code will 
     no longer work, and simply served as a way to initially manage the dataset.
     
@@ -23,22 +23,22 @@ import shutil
 # %% Tidy dataset
 
 #Navigate to dataset folder
-os.chdir('..\\Fukuchi2017-RunningDataset')
+os.chdir('..\\Fukuchi2018-WalkingDataset')
 
 #Identify subject list based on filenames
 #Get file list of static trials
 staticTrials = list()
-for file in glob.glob('*static.c3d'):
+for file in glob.glob('*static*.c3d'):
     staticTrials.append(file)
     
 #Loop through static trials and get participant list
 subList = list()
 for ii in range(len(staticTrials)):
     #Split string and append to list
-    subList.append(staticTrials[ii].split('static.c3d')[0])
+    subList.append(staticTrials[ii].split('static')[0])
     
 #Loop through participants and allocate files to folder
-for ii in range(len(subList)):
+for ii in range(0,len(subList)):
     
     #Create blank list for files
     subFiles = list()
@@ -54,5 +54,4 @@ for ii in range(len(subList)):
     for ff in range(len(subFiles)):
         shutil.move(subFiles[ff],subList[ii]+'\\'+subFiles[ff])
     
-# %% ----- End of tidyFukuchi2017-Running.py -----
-    
+# %% ----- End of tidyFukuchi2018-Walking.py -----
